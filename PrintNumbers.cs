2656d48336ecc2e569153abb5c89cbe88d3f6b0e
Keerthika-Moorthy <meetkeerthika@gmail.com>
2020-07-21 17:01:10 +0530

    Initial commit

---
+++ PrintNumbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventAsync
+{
+    class PrintNumbers
+    {
+        public EventHandler<int> Progress;
+        public void DoWorkBasedOnCount(int count)
+        {
+            for(int i =0;i<count;i++)
+            {
+                if(Progress != null)
+                {
+                    Progress(this, i);
+                }
+            }
+
+        }
+        public async void PrintNumberonCount(int count)
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    Console.WriteLine($"Value:{i.ToString()}");
+
+                }
+            }
+                );
+            Console.WriteLine("Printing Completed");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace EventAsync
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainMethod();  
+        }
+        public static void MainMethod()
+        {
+            PrintNumbers pn = new PrintNumbers();
+            pn.Progress += new EventHandler<int>((s, e) => { Console.WriteLine($"Sender: {s.ToString()}:value:{e.ToString()}"); });
+            Console.WriteLine("Event Handling");
+            pn.DoWorkBasedOnCount(10);
+            Console.WriteLine("Async Await");
+            pn.PrintNumberonCount(10);
+            Console.WriteLine("Main Method: End");
+            Console.ReadLine();
+        }
+    }
+}
